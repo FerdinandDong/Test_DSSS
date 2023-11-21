ee57388bf3bcb9573fc11f8e4b6da395772c9edd
FerdinandDong <ferdinand779053330@gmail.com>
2023-11-21 17:35:35 +0100

    Make repository pip installable

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Test_DSSS',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # 这里可以列出任何依赖，例如 'numpy>=1.13.3'
+    ],
+    entry_points={
+        'console_scripts': [
+            'math_quiz=Test_DSSS.math_quiz:main_function'
+        ]
+    }
+)
